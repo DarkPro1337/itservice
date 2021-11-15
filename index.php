8ca231c505e9d7814a9784240b617653db0ff683
Artem Darkov <darkprothekun@gmail.com>
2021-11-15 10:26:01 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Main template file.
+ * 
+ * @package IT Service
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ИТ Сервис Котлас</title>
+</head>
+<body>
+    
+</body>
+</html>
